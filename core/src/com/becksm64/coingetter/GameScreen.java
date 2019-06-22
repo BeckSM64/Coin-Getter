dea7ea6501e2b14d22a9e9e71e0ff0a0faae24d7
Shanebeck4 <becksm64@gmail.com>
2019-06-22 18:11:29 -0400

    Added coins to the game screen
    A random number of coins are now generated on the game screen

--- core/src/com/becksm64/coingetter/GameScreen.java
+++ core/src/com/becksm64/coingetter/GameScreen.java
@@ -8,6 +8,10 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class GameScreen implements Screen {
 
     private Game game;
@@ -15,6 +19,8 @@ public class GameScreen implements Screen {
     private OrthographicCamera cam;
     private Vector3 touchPos;//Vector to keep track of where screen was touched
     private Player player;
+    private List<Coin> coinArray;
+    private Random rng;
 
     public GameScreen(Game game) {
 
@@ -24,6 +30,20 @@ public class GameScreen implements Screen {
         cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         touchPos = new Vector3(Gdx.graphics.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f, 0);//Start relatively in center of screen
         player = new Player(touchPos.x, touchPos.y);
+        rng = new Random();
+        coinArray = new ArrayList<Coin>();
+        generateCoins();
+    }
+
+    /*
+     * Generate a random number of coins and add them to the coin array
+     */
+    private void generateCoins() {
+        for(int i = 0; i < rng.nextInt(10); i++)
+            coinArray.add(new Coin(rng.nextInt(Gdx.graphics.getWidth() - (int) Coin.WIDTH),
+                    rng.nextInt(Gdx.graphics.getHeight() - (int) Coin.HEIGHT),
+                    (int) ((rng.nextInt(5)) * Gdx.graphics.getDensity()) + 1,
+                    (int) ((rng.nextInt(5)) * Gdx.graphics.getDensity()) + 1));
     }
 
     @Override
@@ -39,7 +59,10 @@ public class GameScreen implements Screen {
         Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        //Update camera and coins
         cam.update();
+        for(Coin coin : coinArray)
+            coin.update();
 
         //Update touched position
         if(Gdx.input.isTouched()) {
@@ -49,9 +72,11 @@ public class GameScreen implements Screen {
             player.setPosition(touchPos.x, touchPos.y);
         }
 
-        //Draw player
+        //Draw assets
         batch.begin();
-        batch.draw(player.getPlayerImage(), player.getPosition().x, player.getPosition().y, Player.SIZE, Player.SIZE);
+        batch.draw(player.getPlayerImage(), player.getPosition().x, player.getPosition().y, Player.SIZE, Player.SIZE);//Draw player
+        for(Coin coin : coinArray)
+            batch.draw(coin.getCoinImage(), coin.getPosition().x, coin.getPosition().y, Coin.WIDTH, Coin.HEIGHT);
         batch.end();
     }
 
