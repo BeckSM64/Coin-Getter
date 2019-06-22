61bd8c2246c4283011b72ea57693f31bfc56d25c
Shanebeck4 <becksm64@gmail.com>
2019-06-22 09:36:25 -0400

    Added game screen and start button
    Added a game screen where the gameplay will take place and a start button to the main menu screen which loads the game screen.

---
+++ core/src/com/becksm64/coingetter/GameScreen.java
@@ -0,0 +1,61 @@
+package com.becksm64.coingetter;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+
+public class GameScreen implements Screen {
+
+    private Game game;
+    private OrthographicCamera cam;
+
+    public GameScreen(Game game) {
+
+        this.game = game;
+        cam = new OrthographicCamera();
+        cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+        //Clear screen with specified color
+        Gdx.gl.glClearColor(0.008f, 0.15f, 0.38f, 1);
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        cam.update();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
--- core/src/com/becksm64/coingetter/MainMenuScreen.java
+++ core/src/com/becksm64/coingetter/MainMenuScreen.java
@@ -4,17 +4,19 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,12 +26,13 @@ public class MainMenuScreen implements Screen {
 
     private Game game;
     private SpriteBatch batch;
-    private Camera cam;
+    private OrthographicCamera cam;
     private Stage stage;
     private Table table;
     private Skin skin;
-    private BitmapFont font;
+    private BitmapFont font, font2;
     private Label gameTitle;
+    private TextButton startBtn;
     private Random rng;
     private Music music;
     private List<Coin> coinArray;
@@ -39,28 +42,36 @@ public class MainMenuScreen implements Screen {
         this.game = game;
         batch = new SpriteBatch();
         cam = new OrthographicCamera();
-        ((OrthographicCamera) cam).setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
         //Setup stage and table for menu
         stage = new Stage();
         table = new Table();
         table.setFillParent(true);//Make table fill stage
         stage.addActor(table);
+        Gdx.input.setInputProcessor(stage);//Set ability to take input on stage
 
         //Setup fonts for menu text and buttons
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/cour.TTF"));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
         parameter.size = (int) (50 * Gdx.graphics.getDensity());
         font = generator.generateFont(parameter);
+        parameter.size = (int) (30 * Gdx.graphics.getDensity());
+        font2 = generator.generateFont(parameter);
         generator.dispose();//Get rid of generator after done making fonts
 
         //Setup menu text and buttons
         skin = new Skin(Gdx.files.internal("skins/uiskin.json"));
         gameTitle = new Label("[COIN GETTER]", skin);
         gameTitle.setStyle(new Label.LabelStyle(font, Color.WHITE));
+        TextButton.TextButtonStyle buttonStyle = new TextButton.TextButtonStyle();
+        buttonStyle.font = font2;
+        startBtn = new TextButton("START", buttonStyle);
 
         //Add texts and buttons to the table
         table.add(gameTitle);
+        table.row();//Next row
+        table.add(startBtn).padTop(10 * Gdx.graphics.getDensity());
 
         //Create and start background music
         rng = new Random();
@@ -85,6 +96,13 @@ public class MainMenuScreen implements Screen {
     @Override
     public void show() {
 
+        //Button listener for start button
+        startBtn.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                game.setScreen(new GameScreen(game));
+            }
+        });
     }
 
     @Override
@@ -133,6 +151,7 @@ public class MainMenuScreen implements Screen {
     @Override
     public void dispose() {
         font.dispose();
+        font2.dispose();
         skin.dispose();
         stage.dispose();
         batch.dispose();
