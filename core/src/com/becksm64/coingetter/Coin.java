f0d285de38e193db60767d69d3df21132fa85e2b
Shanebeck4 <becksm64@gmail.com>
2019-06-22 09:03:36 -0400

    Added bounds and fixed size to coin
    Coins now have a fixed size relative to the screen size and have bounds which are used for collision with other objects and screen edges.

--- core/src/com/becksm64/coingetter/Coin.java
+++ core/src/com/becksm64/coingetter/Coin.java
@@ -1,19 +1,45 @@
 package com.becksm64.coingetter;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class Coin {
 
+    public static final float WIDTH = Gdx.graphics.getWidth() / 10.0f;
+    public static final float HEIGHT = Gdx.graphics.getHeight() / 6.0f;
+
     private Texture coinImage;
     private Vector3 position;
     private Vector3 velocity;
+    private Rectangle bounds;
 
     public Coin(int x, int y, int velX, int velY) {
 
         coinImage = new Texture("sprites/coin.png");
         position = new Vector3(x, y, 0);
         velocity = new Vector3(velX, velY, 0);
+        bounds = new Rectangle(x, y, WIDTH, HEIGHT);
+    }
+
+    public void update() {
+        position.add(velocity.x, velocity.y, 0);
+        setBounds(position.x, position.y);
+        constrainCoin();
+    }
+
+    public void constrainCoin() {
+
+        if(position.x > Gdx.graphics.getWidth() - WIDTH || position.x < 0) {
+            velocity.x *= -1;
+        } else if(position.y > Gdx.graphics.getHeight() - HEIGHT || position.y < 0) {
+            velocity.y *= -1;
+        }
+    }
+
+    private void setBounds(float x, float y) {
+        bounds.setPosition(x, y);
     }
 
     public Texture getCoinImage() {
