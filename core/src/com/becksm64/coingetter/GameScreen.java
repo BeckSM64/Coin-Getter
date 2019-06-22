d8bac874ffb520f4d245955950ef7d1769656ec6
Shanebeck4 <becksm64@gmail.com>
2019-06-22 09:59:40 -0400

    Added player to game screen
    Player is drawn on play screen and its position is set to the current touch position on the screen. Desktop will eventually have keyboard controls.

--- core/src/com/becksm64/coingetter/GameScreen.java
+++ core/src/com/becksm64/coingetter/GameScreen.java
@@ -5,17 +5,25 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
 
 public class GameScreen implements Screen {
 
     private Game game;
+    private SpriteBatch batch;
     private OrthographicCamera cam;
+    private Vector3 touchPos;//Vector to keep track of where screen was touched
+    private Player player;
 
     public GameScreen(Game game) {
 
         this.game = game;
+        batch = new SpriteBatch();
         cam = new OrthographicCamera();
         cam.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        touchPos = new Vector3(Gdx.graphics.getWidth() / 2.0f, Gdx.graphics.getHeight() / 2.0f, 0);//Start relatively in center of screen
+        player = new Player(touchPos.x, touchPos.y);
     }
 
     @Override
@@ -32,6 +40,19 @@ public class GameScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         cam.update();
+
+        //Update touched position
+        if(Gdx.input.isTouched()) {
+
+            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            cam.unproject(touchPos);//Gets correct touch position relative to camera
+            player.setPosition(touchPos.x, touchPos.y);
+        }
+
+        //Draw player
+        batch.begin();
+        batch.draw(player.getPlayerImage(), player.getPosition().x, player.getPosition().y, Player.SIZE, Player.SIZE);
+        batch.end();
     }
 
     @Override
@@ -56,6 +77,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        player.dispose();
+        batch.dispose();
     }
 }
